8e200ea91d2af12777d06ad3dd8e701c87d0ea1f
Peer Ligthart <info@peerligthart.com>
2020-08-06 17:06:06 +0200

    Update hitmarker.js

--- hitmarker/hitmarker.js
+++ hitmarker/hitmarker.js
@@ -35,7 +35,8 @@ var queue = [];
 const enabled = UI.AddCheckbox("Custom hitmarkers");
 const size = UI.AddSliderInt("Hitmarker size", 1, 14);
 const color = UI.AddColorPicker("Hitmarker color");
-const hit = UI.AddCheckbox("Hitmarker on hit only")
+const duration = UI.AddCheckbox("Hitmarker duration", 1, 8);
+const hit = UI.AddCheckbox("Hitmarker on hit only");
 
 //menu handle
 function handle_menu() {
@@ -43,6 +44,7 @@ function handle_menu() {
 
     UI.SetEnabled(("Script items", "Hitmarker size"), script_state);
     UI.SetEnabled(("Script items", "Hitmarker color"), script_state);
+    UI.SetEnabled(("Script items", "Hitmarker duration"), script_state);
     UI.SetEnabled(("Script items", "Hitmarker on hit only"), script_state);
 }
 handle_menu();
@@ -52,28 +54,19 @@ function ragebot_fire_handler() {
     if (Get(hit))
         return;
 
-    var ragebot_target = Event.GetInt("target_index");
-    var ragebot_target_hitbox = Event.GetInt("hitbox");
-    var hitbox_position = Entity.GetHitboxPosition(ragebot_target, ragebot_target_hitbox);
-
-    queue[Globals.Tickcount()] = [hitbox_position[0], hitbox_position[1], hitbox_position[2], Globals.Curtime() + 2];
+    const hitbox_position = Entity.GetHitboxPosition(Event.GetInt("target_index"), Event.GetInt("hitbox"));
+    queue[Globals.Tickcount()] = [hitbox_position[0], hitbox_position[1], hitbox_position[2], Globals.Curtime() + Get(duration)];
 }
 
 function player_hurt_handler() {
     if (!Get(hit))
         return;
 
-    var player_hurt_userid = Event.GetInt("userid");
-    var player_hurt_attackerid = Event.GetInt("attacker");
-    var player_hurt_hitbox = Event.GetInt("hitgroup");
+    const player_hurt_victim = Entity.GetEntityFromUserID(Event.GetInt("userid"));
+    const hitbox_position = Entity.GetHitboxPosition(player_hurt_victim, Event.GetInt("hitgroup"));
 
-    var player_hurt_victim = Entity.GetEntityFromUserID(player_hurt_userid);
-    var player_hurt_attacker = Entity.GetEntityFromUserID(player_hurt_attackerid);
-
-    var hitbox_position = Entity.GetHitboxPosition(player_hurt_victim, player_hurt_hitbox);
-
-    if (player_hurt_victim != Entity.GetLocalPlayer() && player_hurt_attacker == Entity.GetLocalPlayer())
-        queue[Globals.Tickcount()] = [hitbox_position[0], hitbox_position[1], hitbox_position[2], Globals.Curtime() + 2];
+    if (player_hurt_victim != Entity.GetLocalPlayer() && Entity.GetEntityFromUserID(Event.GetInt("attacker")) == Entity.GetLocalPlayer())
+        queue[Globals.Tickcount()] = [hitbox_position[0], hitbox_position[1], hitbox_position[2], Globals.Curtime() + Get(duration)];
 }
 
 function draw_handler() {
@@ -82,16 +75,14 @@ function draw_handler() {
     if (!Get(enabled))
         return;
 
+    const hm_color = UI.GetColor("Misc", "JAVASCRIPT", "Script items", "Hitmarker color");
+
     for (var i in queue) {
-        if (Globals.Curtime() <= queue[i][3]) {
-            var pos_2d = Render.WorldToScreen([queue[i][0], queue[i][1], queue[i][2]]);
-
-            if (pos_2d[0] != null && pos_2d[1] != null) {
-                const hm_color = UI.GetColor("Misc", "JAVASCRIPT", "Script items", "Hitmarker color")
-
-                Render.Line(pos_2d[0] - Get(size), pos_2d[1], pos_2d[0] + Get(size), pos_2d[1], hm_color);
-                Render.Line(pos_2d[0], pos_2d[1] - Get(size), pos_2d[0], pos_2d[1] + Get(size), hm_color);
-            }
+        var pos_2d = Render.WorldToScreen([queue[i][0], queue[i][1], queue[i][2]]);
+        
+        if (pos_2d[2] != null && Globals.Curtime() <= queue[i][3]) {
+            Render.Line(pos_2d[0] - Get(size), pos_2d[1], pos_2d[0] + Get(size), pos_2d[1], hm_color);
+            Render.Line(pos_2d[0], pos_2d[1] - Get(size), pos_2d[0], pos_2d[1] + Get(size), hm_color);
         }
     }
 }
@@ -109,6 +100,7 @@ function init() {
         return;
 
     Set(size, 7);
+    Set(duration, 2);
     UI.SetColor("Misc", "JAVASCRIPT", "Script items", "Hitmarker color", [255, 255, 255, 255]);
     
     Cheat.RegisterCallback("ragebot_fire", "ragebot_fire_handler");
